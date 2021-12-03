3365af7a7266e2764e7cb6c3c5e4fd836de397e0
Wilson Cruz <galatoide@gmail.com>
2021-12-03 18:30:50 +0000

    Create cookie button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+
 function App() {
+	const [count, setCount] = useState(0);
+
 	return (
-		<div className='App'>
-			<h1>Test</h1>
+		<div>
+			<div
+				onClick={() => {
+					setCount(count + 1);
+					console.log(count);
+				}}
+				className='App'>
+				<h1>Cookie</h1>
+			</div>
+			<h1>{count}</h1>
 		</div>
 	);
 }
