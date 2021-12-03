97e3e4700219bb6693af29d63389c787e02f24a3
Wilson Cruz <galatoide@gmail.com>
2021-12-03 18:39:13 +0000

    add reset button

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@ function App() {
 
 	return (
 		<div>
-			<div
+			{/* Cookie Button */}
+			<button
 				onClick={() => {
 					setCount(count + 1);
 					console.log(count);
 				}}
 				className='App'>
 				<h1>Cookie</h1>
-			</div>
+			</button>
+
 			<h1>{count}</h1>
+
+			{/* Reset Button */}
+			<button
+				onClick={() => {
+					setCount(0);
+					console.log(count);
+				}}
+				className='App'>
+				<p>Reset</p>
+			</button>
 		</div>
 	);
 }
