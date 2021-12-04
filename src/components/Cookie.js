90f3286a4df8155fd49dc680edd8085b446ea9ed
Wilson Cruz <galatoide@gmail.com>
2021-12-04 02:04:31 +0000

    cursor upgrade

--- src/components/Cookie.js
+++ src/components/Cookie.js
@@ -6,6 +6,7 @@ import cookie from '../images/cookie.png';
 const Cookies = () => {
 	const [count, setCount] = useState(0); //Counter
 	const [clickSound] = useSound(click); //Click Sound
+	const [clicks, setClicks] = useState(1); // the clicks counter
 
 	return (
 		<div className='container'>
@@ -16,8 +17,8 @@ const Cookies = () => {
 					className='cookie'
 					alt='cookie'
 					onClick={() => {
-						setCount(count + 1);
-						console.log(count);
+						setCount(count + clicks);
+						// console.log(count);
 					}}
 					onMouseDown={clickSound}
 				/>
@@ -26,13 +27,24 @@ const Cookies = () => {
 
 			<h1>Bakery: {count}</h1>
 
+			{/* Cursor Button */}
+			<button
+				onClick={() => {
+					setClicks(clicks + 2);
+					// console.log(clicks);
+				}}
+				className='App'>
+				<p>Cursor</p>
+			</button>
+
 			{/* Reset Button */}
 			<button
 				onClick={() => {
 					setCount(0);
+					setClicks(0);
 					console.log(count);
 				}}
-				className='App'>
+				className='reset'>
 				<p>Reset</p>
 			</button>
 		</div>
